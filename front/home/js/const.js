9eaa7b59b752c17bbf172da455bf639649b5d35b
cscsb <49179157+cscsb@users.noreply.github.com>
2019-11-05 17:10:58 +0800

    Add files via upload

---
+++ front/home/js/const.js
@@ -0,0 +1,37 @@
+
+
+ var websiteurl ="http://ppssii.com"
+
+
+/*查询分类*/
+var getMenuUrl = 'websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=getMenu';
+
+/*添加分类*/
+var addMenuUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=addMenu";
+
+/*删除分类*/
+var deleteMenuUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=deleteMenu";
+
+/*根据分类查询item*/
+var getCatyitemUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=getCatyitem";
+
+/*添加item*/
+var addItemUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=addItem";
+
+/*修改item*/
+var setItemUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=setItem";
+
+/*删除item*/
+var deleteItemUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=deleteItem";
+
+/*归类item到分类*/
+var mvItemUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=mvItem";
+
+/*查询图片接口*/
+var getImageUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=getImages";
+
+/*上传图片接口*/
+var uploadImagesUrl = "websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=uploadImages";
+
+/*删除图片接口*/
+var deleteImagesUrl ="websiteurl+/plugin.php?id=xiaomy_cus_todo&mod=deleteImages";
